39f92a87f88b7199be74fda74660ab7dba8671e8
SeungjunNa <nsjjsng1@gmail.com>
2023-01-17 17:13:14 +0900

    blog database 생성

---
+++ blog/src/main/java/com/nsj/blog/model/User.java
@@ -0,0 +1,46 @@
+package com.nsj.blog.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder // 빌더패턴!
+//ORM -> Java(다른언어) Object -> 테이블로 매핑해주는 기술
+@Entity // User클래스가 MySQL에 테이블이 생성이된다.
+public class User {
+
+		@Id // Primary Key
+		@GeneratedValue(strategy = GenerationType.IDENTITY) //프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
+		private int id; //시퀀스, auto_increment
+		
+		@Column(nullable=false, length=30)
+		private String username; // 아이디
+		
+		@Column(nullable=false, length=100)	//1234 => 해쉬 (비밀번호 암호화)
+		private String password;
+		
+		@Column(nullable=false, length=50)
+		private String email;
+		
+		@ColumnDefault("'user'")
+		private String role; // Enum을 쓰는게 좋다. // admin, user, manager 도메인 설정해 3가지중 하나로
+		
+		@CreationTimestamp // 시간이 자동 입력
+		private Timestamp createDate;
+		
+}

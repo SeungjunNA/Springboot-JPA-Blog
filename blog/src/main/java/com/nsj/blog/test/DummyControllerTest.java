3d35028a52c998c01c2e82beb573f3d9158d5295
SeungjunNa <nsjjsng1@gmail.com>
2023-01-18 21:07:20 +0900

    blog 화면 만들기

--- blog/src/main/java/com/nsj/blog/test/DummyControllerTest.java
+++ blog/src/main/java/com/nsj/blog/test/DummyControllerTest.java
@@ -6,10 +6,12 @@ import java.util.function.Supplier;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,11 +30,22 @@ public class DummyControllerTest {
 	@Autowired //의존성 주입(DI)
 	private UserRepository userRepository;
 	
+	@DeleteMapping("/dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		try {
+			userRepository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			return "삭제에 실해하였습니다. 해당 id는 DB에 없습니다.";
+		}
+		return "삭제되었습니다. id : "+id;
+	}
+	
+	
 	// save함수는 id를 전달하지 않으면 insert해주고
 	// save함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고
 	// save함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 해줌.
 	// email, password
-	@Transactional
+	@Transactional //함수 종료시 자동 commit //더티체킹
 	@PutMapping("/dummy/user/{id}")
 	public User updateUser(@PathVariable int id,@RequestBody User requestUser) { // json 데이터를 요청 => Java Object(MessageConverter의 Jackson라이브러리가 변환해서 받아줌.)
 		System.out.println("id : "+ id);
@@ -47,8 +60,7 @@ public class DummyControllerTest {
 		
 		//userRepository.save(user);
 		
-		// 더티 체킹
-		return null;
+		return user;
 	}
 	
 	// http://localhost:8000/blog/dummy/user
